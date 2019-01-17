b1edde36e3ee9cd103982e5c7fda613e25bef880
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-17 12:49:20 +0000

    removed const and replace with var in peeps.js

--- test/feature/peeps.js
+++ test/feature/peeps.js
@@ -3,9 +3,9 @@ var app = require('../../server');
 var expect = require('expect.js')
 Browser.localhost('example.com', 3000);
 
-describe('User visits signup page', function() {
+describe('User visits homepage', function() {
 
-  const browser = new Browser();
+  var browser = new Browser();
 
   before(function() {
     return browser.visit('/');
@@ -13,7 +13,7 @@ describe('User visits signup page', function() {
 
   describe('homepage', function() {
     it('should see welcome page', function() {
-      expect(browser.text("title")).to.equal('Chitter Api Tech Test');
+      expect(browser.text("title")).to.equal('Chitter Api Tech Test')
     });
   });
 
