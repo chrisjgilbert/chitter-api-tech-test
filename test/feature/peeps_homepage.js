e24607851d55506208a183422778eb6df2383740
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-18 12:20:45 +0000

    done callback added

--- test/feature/peeps_homepage.js
+++ test/feature/peeps_homepage.js
@@ -12,17 +12,20 @@ describe('User visits homepage', function() {
   });
 
   describe('peeps homepage', function() {
-    it('should display title', function() {
+    it('should display title', function(done) {
       expect(browser.text("title")).to.equal('Chitter Api Tech Test')
+      done()
     });
 
-    it('should display page h1', function() {
+    it('should display page h1', function(done) {
       expect(browser.text("h1")).to.equal('Peepline')
+      done()
     });
 
-    it('should display list of all peeeps with most recent peep first', function() {
+    it('should display list of all peeeps with most recent peep first', function(done) {
       expect(browser.text("li:nth-of-type(1)")).to.equal('Hit em up? More like hit me up (pls)')
       expect(browser.text("li:nth-of-type(2)")).to.equal('guess who?')
+      done()
     });
   });
 
